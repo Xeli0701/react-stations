2eb25d05439bdcd9a7a7893bf39a26f30f9012cd
Xeli0701 <yamato232@live.jp>
2021-06-18 21:45:01 +0900

    station4 attempt1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from "react";
 import './App.css'
 
 /**
  * 
  * @type {React.FC}
  */
+
 export const App = () => {
+  const [dogURL, setDogUrl] = useState("https://images.dog.ceo/breeds/cairn/n02096177_1596.jpg");
   return (
     <main role="main">
       <header>Test Flight!</header>
@@ -16,7 +19,7 @@ export const App = () => {
           This is Dog image.
         </div>
       </div>
-      <img src="https://images.dog.ceo/breeds/cairn/n02096177_1596.jpg" alt="dog"></img>
+      <img src={dogURL} alt="dog"></img>
     </main>
   )
 }
