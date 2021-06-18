4f728c3e56cd280918436e98af6ca0d1fd6bd08f
Xeli0701 <yamato232@live.jp>
2021-06-19 00:21:26 +0900

    station7 modified App.css

--- src/App.js
+++ src/App.js
@@ -15,13 +15,15 @@ export const App = () => {
 
   return (
     <main role="main">
-      <header>Test Flight!</header>
-      <div>
-        This is Dog image.
-      </div>
-      <img src={dogURL} alt="dog"></img>
-      <div>
-        <button
+      <header className="site-header">
+        <h1>Test Flight!</h1>
+        <nav class="dog-nav">
+          Automatic Dog image Display powerd by dog.
+        </nav>
+      </header>
+      <div className="dog-container">
+        <img className="dog-image" src={dogURL} alt="dog"></img>
+        <button className="btn-flat-border"
           onClick={() => fetch("https://dog.ceo/api/breeds/image/random")
             .then(response => response.json())
             .then((data) => {
