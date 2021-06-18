b60dc016e8d763288623ba798dd5bf94e1035999
Xeli0701 <yamato232@live.jp>
2021-06-18 22:27:23 +0900

    station6 attempt1

--- src/App.js
+++ src/App.js
@@ -11,23 +11,26 @@ import './App.css'
 
 export const App = () => {
   const [dogURL, setDogUrl] = useState("https://images.dog.ceo/breeds/cairn/n02096177_1596.jpg");
-
-  const dogurl_change = () => {
-    setDogUrl = "https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg";
-  }
+  let getRandomDog = () => { console.log() };
 
   return (
     <main role="main">
       <header>Test Flight!</header>
       <div>
-        <div>
-          This is Dog image.
-        </div>
+        This is Dog image.
       </div>
       <img src={dogURL} alt="dog"></img>
-      <div class="button1">
-        <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>Press to Change Dog</button>
+      <div>
+        <button
+          onClick={() => fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then((data) => {
+              if (data.status === "success") {
+                setDogUrl(data.message)
+              }
+            })}
+        >Press to Change Dog</button>
       </div>
     </main>
-  )
+  );
 }
